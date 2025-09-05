4df0dc33b610f43a4cf7f7f93fe8ab279fe4e5b1
Atharva Bakale <atharvabakale13@gmail.com>
2025-09-05 17:45:35 +0530

    Update main.py
    Added Playing Music, Opening Stack Overflow, Opening YouTube, Opening Google and Telling The Time Functionality.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import pyttsx3 # type: ignore
 import datetime # type: ignore
 import speech_recognition as sr # type: ignore
+import webbrowser # type: ignore
+import os # type: ignore
+import random # type: ignore
 
 # Configuring Engine
 engine = pyttsx3.init('sapi5') # Using Default Speech API
@@ -48,7 +51,38 @@ def takeCommand():
     except Exception as e:
       print("Say That Again Please...") # Prompt User to Repeat For Recognition Failure
       return "None"
+  
+    return query # Return Recognized Text
 
 # Main Function
 if __name__ == "__main__":
   wishMe() # Call WishMe Function
+
+  while True:
+    query = takeCommand().lower() # Call takeCommand Function and Convert to Lowercase
+
+    # Logic for Executing Tasks Based on Query
+
+    # Open In Browser
+    if "open youtube" in query: # YouTube
+      webbrowser.open("youtube.com")
+
+    elif "open google" in query: # Google
+      webbrowser.open("google.com")
+
+    elif "open stackoverflow" in query: # Stack Overflow
+      webbrowser.open("stackoverflow.com")
+
+    # Playing Music
+    elif "play music" in query:
+      musicDir = "C:\\Users\\nitin\\OneDrive\\Music" # Path to Music Directory
+      songs = os.listdir(musicDir) # List All Songs in Directory
+      
+      random_index = random.randint(0, len(songs) - 1) # Generate Random Index
+
+      os.startfile(os.path.join(musicDir, songs[random_index])) # Start Playing the Randomly Selected Song
+
+    # Telling Time
+    elif "the time" in query:
+      strTime = datetime.datetime.now().strftime("%H:%M:%S") # Get Current Time
+      print (strTime) # Print Current Time
